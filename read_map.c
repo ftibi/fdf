63bcbe2c93050315daff26f94c3045dd7d413167
Thibault FOLLY <tfolly@e3r5p9.42.fr>
2016-04-07 14:03:59 +0200

    gnl

--- read_map.c
+++ read_map.c
@@ -6,13 +6,24 @@
 /*   By: tfolly <tfolly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/05 18:08:11 by tfolly            #+#    #+#             */
-/*   Updated: 2016/04/05 19:23:30 by tfolly           ###   ########.fr       */
+/*   Updated: 2016/04/07 14:03:40 by tfolly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 #include "libft/includes/libft.h"
-#include "get_next_line.h"
+#include "getnextline/get_next_line.h"
 
-
+int	main(int ac, char *av)
+{
+	int 	lec;
+	char	*tmp;
+	lec = 1;
+	tmp = NULL;
+	while (lec)
+	{
+		lec = get_next_line();
+	}
+	return (0);
+}
 
